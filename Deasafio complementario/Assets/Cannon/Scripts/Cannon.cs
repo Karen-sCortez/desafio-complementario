f29936a81f68c2ef5d6e8eeb9b87c0e8a22acae7
Karen-sCortez <karen_silveracortez@hotmail.com>
2022-10-13 13:53:39 -0300

    V 0.01

---
+++ Deasafio complementario/Assets/Cannon/Scripts/Cannon.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+    public GameObject bala;
+    public Transform SalidaDeBala;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+		{
+			Shoot();
+		}
+    }
+	
+	private void Shoot()
+	{
+		Debug.Log("The Cannon has been shoot!");
+        Instantiate(objetoAInstanciar, posicionAInstanciar, transform.rotation);
+    }
+}
